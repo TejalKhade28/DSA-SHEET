79647ee27987225539014d7934223f30a3f54584
Tejal Khade <79683880+TejalKhade28@users.noreply.github.com>
2022-07-16 12:16:23 +0530

    first commit

---
+++ repeatednumber.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,count=0;
+    cout<<"Enter the number of terms ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+     cin>>arr[i];   
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+        	if(arr[i]==arr[j]){
+        		cout<<arr[i]<<" ";
+               
+            }
+			
+		}
+    }
+    
+    return 0;
+}
